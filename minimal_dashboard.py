abf0484d0b3422b9d73c8edf91c37f1f6f3b7e03
Romain PRIMET <romain.primet@inria.fr>
2020-01-29 14:23:25 +0100

    layout fixes

--- minimal_dashboard.py
+++ minimal_dashboard.py
@@ -44,11 +44,11 @@ def run_and_display_query(_):
     similar_docs = result["similar_documents"].head().copy()
     similar_docs.loc[:, 'title'] = title_as_link(similar_docs)
     
-    bmap_out = widgets.Output(layout=widgets.Layout(width='auto', min_width='200px', height='200px', grid_area='brainmap'))
+    bmap_out = widgets.Output(layout=widgets.Layout(width='auto', height='200px', grid_area='brainmap'))
     terms_out = widgets.Output(layout=widgets.Layout(width='auto', height='350px', grid_area='terms'))
     docs_out = widgets.Output(layout=widgets.Layout(width='auto', grid_area='docs'))
     
-    bmap_out.append_display_data(HTML(view_img(result["z_map"], threshold=3.1).get_iframe(), 350, 120))
+    bmap_out.append_display_data(HTML(view_img(result["z_map"], threshold=3.1).get_iframe()))
     terms_out.append_display_data(Markdown("## Similar Words"))
     sw = result["similar_words"].head(12)
     terms_out.append_display_data(sw.style.bar(subset=['weight_in_brain_map', 'weight_in_query'], color='lightgreen'))
@@ -66,7 +66,7 @@ def run_and_display_query(_):
                     grid_template_rows='auto auto',
                     grid_template_columns='50% 50%',
                     grid_template_areas='''
-                    "braimap terms"
+                    "brainmap terms"
                     "docs    docs"
                 ''')
             )
