5a70d9bf85b694cfe7a76e02f75938b1761ab264
Romain PRIMET <romain.primet@inria.fr>
2020-01-29 13:42:40 +0100

    interim commit

--- minimal_dashboard.py
+++ minimal_dashboard.py
@@ -25,7 +25,7 @@
 from neuroquery import fetch_neuroquery_model, NeuroQueryModel
 from nilearn.plotting import plot_img, view_img
 import ipywidgets as widgets
-from IPython.display import display, display_html, Markdown
+from IPython.display import display, HTML, Markdown
 
 # %%capture
 encoder = NeuroQueryModel.from_data_dir(fetch_neuroquery_model())
@@ -33,8 +33,8 @@ encoder = NeuroQueryModel.from_data_dir(fetch_neuroquery_model())
 query = widgets.Text(value="brainstem")
 button = widgets.Button(description="Run query")
 display(widgets.HBox([query, button]))
-output = widgets.Output()
-display(output)
+main_output = widgets.Output()
+display(main_output)
 
 def title_as_link(df):
     return df.apply(lambda x: f"<a href=\"{x['pubmed_url']}\" target=\"_blank\">{x['title']}</a>", axis=1)
@@ -43,15 +43,37 @@ def run_and_display_query(_):
     result = encoder(query.value)
     similar_docs = result["similar_documents"].head().copy()
     similar_docs.loc[:, 'title'] = title_as_link(similar_docs)
-    with output:
-        output.clear_output()
-        display_html(view_img(result["z_map"], threshold=3.1).get_iframe(), raw=True)
-        sw = result["similar_words"].head(12)
-        display(Markdown("## Similar Words"))
-        display(sw.style.bar(subset=['weight_in_brain_map', 'weight_in_query'], color='lightgreen'))
-        display(Markdown("## Similar Documents"))
-        display(similar_docs[['title', 'similarity']].style.hide_index().bar(color='lightgreen'))
+    
+    bmap_out = widgets.Output(layout=widgets.Layout(width='auto', min_width='200px', height='200px', grid_area='brainmap'))
+    terms_out = widgets.Output(layout=widgets.Layout(width='auto', height='350px', grid_area='terms'))
+    docs_out = widgets.Output(layout=widgets.Layout(width='auto', grid_area='docs'))
+    
+    bmap_out.append_display_data(HTML(view_img(result["z_map"], threshold=3.1).get_iframe(), 350, 120))
+    terms_out.append_display_data(Markdown("## Similar Words"))
+    sw = result["similar_words"].head(12)
+    terms_out.append_display_data(sw.style.bar(subset=['weight_in_brain_map', 'weight_in_query'], color='lightgreen'))
+    docs_out.append_display_data(Markdown("## Similar Documents"))   
+    docs_out.append_display_data(similar_docs[['title', 'similarity']].style.hide_index().bar(color='lightgreen'))
+
+    with main_output:
+        main_output.clear_output()
+        
+        display(
+            widgets.GridBox(
+                children=[bmap_out, terms_out, docs_out],
+                layout=widgets.Layout(
+                    width='100%',
+                    grid_template_rows='auto auto',
+                    grid_template_columns='50% 50%',
+                    grid_template_areas='''
+                    "braimap terms"
+                    "docs    docs"
+                ''')
+            )
+        )
 
 button.on_click(run_and_display_query)
 
 run_and_display_query(None)
+
+
